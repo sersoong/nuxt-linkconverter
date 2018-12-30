8052177cdf424a5a3c34277fbb77ab9dba8ef790
sersoong <sersoong@gmail.com>
2018-12-30 17:59:13 +0800

    修改font

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,8 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
+      { rel:'stylesheet', href:"https://use.fontawesome.com/releases/v5.0.13/css/all.css"},
+      { rel: 'stylesheet', href: 'https://fonts.cat.net/css?family=Roboto:300,400,500,700|Material+Icons' }
     ]
   },
 
