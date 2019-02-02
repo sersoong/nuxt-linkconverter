840096c8759ad6beb91a1d03f735c02fec069bfe
sersoong <sersoong@gmail.com>
2019-02-02 12:07:11 +0800

    实现基本功能

--- components/converter.vue
+++ components/converter.vue
@@ -5,7 +5,12 @@
     wrap
     >
       <v-flex xs4>
-        <v-btn color="info">Convert</v-btn>
+        <v-btn 
+        color="info"
+        @click="convert"
+        >
+          Convert
+        </v-btn>
         <v-btn
         color="info"
         @click="clear"
@@ -17,7 +22,7 @@
         <v-select
           :items="items"
           label="Type"
-          :value="items[0]"
+          v-model='type'
         ></v-select>
       </v-flex>
       <v-flex xs6>
@@ -34,7 +39,7 @@
         <v-textarea
           name="convert"
           label="Result"
-          value=""
+          :value='result'
           color="blue"
           hint="A result per line"
           auto-grow
@@ -51,8 +56,10 @@ export default{
     name:"tab",
     data () {
       return {
-        "inputvalue":"",
-        items: ['Magnet', 'Baidupan', 'Fizz', 'Buzz']
+        inputvalue:"",
+        items: ['Magnet', 'Baidupan', 'Youtube'],
+        type:'',
+        result:''
       }
     },
     props:{
@@ -60,9 +67,35 @@ export default{
             default:"label"
         }
     },
+    mounted () {
+      this.type = this.items[0]
+    },
     methods: {
       clear: function (){
         this.inputvalue=""
+      },
+      convert: function (){
+        let _type = this.type
+        let _input = this.inputvalue.split("\n")
+        let _result = new Array()
+        for (var i=0;i<_input.length;i++){
+          let temp = Array()
+          if(_type==this.items[0]){
+            temp.push("magnet:?xt=urn:btih:")
+          }
+          else if(_type==this.items[1]){
+            if(_input[i].substring(0,3)=="/s/"){
+              _input[i]=_input[i].slice(3)
+            } 
+            temp.push("https://pan.baidu.com/s/")
+          }
+          else if(_type==this.items[2]){
+            temp.push("https://www.youtube.com/watch?v=")
+          }
+          temp.push(_input[i])
+          _result[i]=temp.join("")
+        }
+        this.result=_result.join("\n")
       }
     }
 }
